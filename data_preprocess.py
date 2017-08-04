3363ef1101fc4d5108e770b3d43a359008d751fe
bobbychovip <bobbychovip@gmail.com>
2017-08-04 17:04:44 +0800

    data processing

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,3 +1,4 @@
+#-*-coding:UTF-8 -*-
 import pandas as pd
 import numpy as np
 
--- input_data.py
+++ input_data.py
@@ -1,3 +1,4 @@
+#-*-coding:UTF-8 -*-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
